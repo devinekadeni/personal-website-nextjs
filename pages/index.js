931188c27efa57b33e59810e35270fc72268a4d6
Devin <devin@getcraft.com>
2020-06-29 00:20:13 +0700

    utilize header on index route

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,47 @@
-import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import Header from 'components/Header'
+import isMobile from 'utils/isMobile'
 
-const index = () => {
-  return <div>Hello World</div>
+const Wrapper = styled.div`
+  position: relative;
+
+  .header-section {
+    position: fixed;
+    width: 100%;
+    top: 0;
+    z-index: 100;
+  }
+
+  .content-section {
+    margin-top: 108px;
+  }
+`
+
+const Index = ({ isMobile }) => {
+  return (
+    <Wrapper>
+      <div className="header-section">
+        <Header
+          // handleScrollIntoView={handleScrollIntoView}
+          // refList={{ aboutEl, skillsEl, contactEl }}
+          // isScrollDown={isScrollDown}
+          // handleBarMenu={setIsScrollDown}
+          // isDarkTheme={isDarkTheme}
+          isMobile={isMobile}
+        />
+      </div>
+      <div className="content-section"></div>
+    </Wrapper>
+  )
 }
 
-export default index
+export function getServerSideProps({ req }) {
+  return { props: { isMobile: isMobile(req) } }
+}
+
+Index.propTypes = {
+  isMobile: PropTypes.bool,
+}
+
+export default Index

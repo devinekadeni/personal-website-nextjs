3a6de2f3192050d926f0cfe99988c404b2695166
Devin Ekadeni <devinekadeni@gmail.com>
2021-12-24 02:22:00 +0700

    config: Enable ISR

--- pages/index.tsx
+++ pages/index.tsx
@@ -200,6 +200,7 @@ export async function getStaticProps() {
         contactRes: contactRes.data,
         skillRes: skillRes.data,
       },
+      revalidate: 120,
     }
   } catch (error) {
     return {

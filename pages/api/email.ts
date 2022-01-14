adfd42974b2d8367c0e32f5d40c26ccae8e3979f
Devin Ekadeni <devinekadeni@gmail.com>
2022-01-14 22:59:50 +0700

    feat: Replace sendgrid API with sendinblue API

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -1,37 +1,62 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios'
-import sgMail from '@sendgrid/mail'
 
-async function postEmail(req: NextApiRequest, res: NextApiResponse) {
-  const { name, message, email } = req.body
-
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY as string)
-
-  const { data: contactData } = await axios.get(`${process.env.API_HOST}/contact`)
-
-  const msg = {
-    to: contactData.data.email,
-    from: email,
-    subject: `Website message from ${name}`,
-    text: 'website',
-    html: `<div>${message}</div>`,
-  }
-
-  try {
-    await sgMail.send(msg)
-    res.status(200).json({ success: true, message: 'Send email success' })
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: `Failed to send email by Sendgrid. Error: ${error}`,
-    })
-  }
+type Params = {
+  name: string
+  email: string
+  message: string
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+type Data = {
+  success: boolean
+  message: string
+  messageId?: string
+}
+
+function sendEmail({ email, name, message }: Params) {
+  const API_KEY = process.env.SENDINBLUE_API_KEY as string
+
+  return axios({
+    method: 'POST',
+    url: `${process.env.SENDINBLUE_API_HOST}/smtp/email`,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'api-key': API_KEY,
+    },
+    data: {
+      sender: { name, email },
+      to: [
+        {
+          name: 'Devin Ekadeni',
+          email: 'devinekadeni@gmail.com',
+        },
+      ],
+      subject: `Website message from ${name}`,
+      textContent: message,
+    },
+  }).then((response) => {
+    return response.data
+  })
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   res.setHeader('Content-Type', 'application/json')
 
   if (req.method === 'POST') {
-    await postEmail(req, res)
+    const { name, email, message } = req.body
+    const payload = {
+      name,
+      email,
+      message,
+    }
+
+    sendEmail(payload)
+      .then(() => {
+        res.status(200).json({ success: true, message: 'Send email success' })
+      })
+      .catch(() => {
+        res.status(500).json({ success: false, message: 'Failed to send email' })
+      })
   }
 }
